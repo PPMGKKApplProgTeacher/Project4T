ab18e056602e87b603cf94098c0ea2891adcbace
ApplProg Teacher <ppmg_kk_appl_prog_teacher@proton.me>
2025-01-08 08:59:53 +0200

    Added DbContext, Project References and ConnectionString

--- Project4T/Program.cs
+++ Project4T/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Project4T.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection,b=>b.MigrationsAssembly("Project4T.DataAccess")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
