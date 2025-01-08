94c247c25e18da2465907902b201172438e8719a
ApplProg Teacher <ppmg_kk_appl_prog_teacher@proton.me>
2025-01-08 11:02:56 +0200

    Added Service structure

---
+++ Project4T.Core/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4T.Core.IServices
+{
+    internal interface IProductService
+    {
+    }
+}
