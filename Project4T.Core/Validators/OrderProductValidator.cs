1d85143e1e4f80a703012bdf48b9a04314875960
ApplProg Teacher <ppmg_kk_appl_prog_teacher@proton.me>
2025-01-15 10:04:14 +0200

    Added Validators

---
+++ Project4T.Core/Validators/OrderProductValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4T.Core.Validators
+{
+    internal class OrderProductValidator
+    {
+    }
+}
