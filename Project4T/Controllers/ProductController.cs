3e469a700b317943191eaf429ee7edd4f9db6631
ApplProg Teacher <ppmg_kk_appl_prog_teacher@proton.me>
2025-01-17 10:52:36 +0200

    Created ProductFilterModelView

--- Project4T/Controllers/ProductController.cs
+++ Project4T/Controllers/ProductController.cs
@@ -1,15 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Project4T.Core.IServices;
 using Project4T.Core.Services;
+using Project4T.Models;
 
 namespace Project4T.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        public ProductController(IProductService  productService, 
+                                 ICategoryService categoryService)
         {
-            return View();
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+        public IActionResult Index(ProductFilterViewModel? filter)
+        {
+            var query = _productService.GetAll().AsQueryable();
+            if (filter != null && filter.CategoryId != null) 
+            {
+                 query = query.Where(x=>x.CategoryId==filter.CategoryId.Value);
+            }
+            if (filter != null && filter.MinPrice != null) 
+            {
+                query = query.Where(x=>x.Price>=filter.MinPrice.Value);
+            }
+            if (filter != null && filter.MaxPrice != null)
+            {
+                query = query.Where(x=>x.Price<=filter.MaxPrice.Value);
+            }
+            var model = new ProductFilterViewModel
+            {
+                CategoryId = filter.CategoryId,
+                MinPrice = filter.MinPrice,
+                MaxPrice = filter.MaxPrice,
+                Categories = new SelectList(_categoryService.GetAll(), "CategoryId", "Name"),
+                Products = query.Include(x => x.Category).ToList()
+            };
+            return View(model);
         }
         public IActionResult Add()
         {
---
+++ Project4T/Models/ProductFilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Project4T.Models
+{
+    public class ProductFilterViewModel
+    {
+        public int? CategoryId { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public decimal? MinPrice { get; set; }
+        public string? Search {  get; set; }    
+        public SelectList Categories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
